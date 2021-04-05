ebe74666926d69d821355628e5f3ed95f7e851c9
yourdilemma <k.iankovskaia@gmail.com>
2021-04-05 18:05:32 -0400

    trying to deploy

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,7 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Navbar } from "react-bootstrap";
 const Nav = () => {
     return (
-           <Navbar class="navbar navbar-expand-lg bg-light navbar-light">
+           <Navbar className="navbar navbar-expand-lg bg-light navbar-light">
                this is nav bar
         </Navbar>
     )
